15a525339e2e11077baec9f4535c70fcc3bcd054
aljo <aljo@novasolution.net>
2020-10-28 22:49:01 +0400

    wide and deep model

--- src/clients/python/examples/wd_client.py
+++ src/clients/python/examples/wd_client.py
@@ -13,7 +13,7 @@ triton_client = httpclient.InferenceServerClient(url=server_url, verbose=False)
 def test_infer(model_name, input0_data, headers=None):
     inputs = []
     outputs = []
-    
+
     inputs.append(httpclient.InferInput('INPUT0__0', [1, 39], "INT32"))
     inputs[0].set_data_from_numpy(input0_data, binary_data=False)
 
@@ -32,7 +32,12 @@ if __name__ == '__main__':
 
     # Create the data for the two input tensors. Initialize the first
     # to unique integers and the second to all ones.
-    input0_data = np.arange(start=0, stop=39, dtype=np.int32)
+    #input0_data = np.arange(start=0, stop=39, dtype=np.int32)
+    input0_data = np.array([   8,    8,   30,   12,   83,   44,   16,    7,   41,    4,   13,
+         16,   18,   81,  167,  284,  881,   32,    7,  200,   12,    0,
+        386, 1246,  393,  213,    8,   33,  731,    4,  647,   57,    1,
+        500,    0,    2,  189,   11,  147], dtype=np.int32)
+
     input0_data = np.expand_dims(input0_data, axis=0)
 
     # Infer with requested Outputs
