fe17aadb9db564cf61058645c9ac6054c70a3999
aljo <aljo@novasolution.net>
2020-10-28 17:09:39 +0400

    input data

--- src/clients/python/examples/wd_client.py
+++ src/clients/python/examples/wd_client.py
@@ -10,18 +10,14 @@ from tritonclient.utils import InferenceServerException
 server_url = 'localhost:8000'
 triton_client = httpclient.InferenceServerClient(url=server_url, verbose=False)
 
-def test_infer(model_name, input0_data, input1_data, headers=None):
+def test_infer(model_name, input0_data, headers=None):
     inputs = []
     outputs = []
+    
     inputs.append(httpclient.InferInput('INPUT0__0', [1, 39], "INT32"))
-
-    # Initialize the data
     inputs[0].set_data_from_numpy(input0_data, binary_data=False)
-    inputs[1].set_data_from_numpy(input1_data, binary_data=True)
 
     outputs.append(httpclient.InferRequestedOutput('OUTPUT0__0', binary_data=True))
-    # outputs.append(httpclient.InferRequestedOutput('OUTPUT1',
-    #                                                binary_data=False))
     query_params = {'test_1': 1, 'test_2': 2}
     results = triton_client.infer(model_name,
                                   inputs,
@@ -40,7 +36,7 @@ if __name__ == '__main__':
     input0_data = np.expand_dims(input0_data, axis=0)
 
     # Infer with requested Outputs
-    results = test_infer(model_name, input0_dat, headers=None)
+    results = test_infer(model_name, input0_data, headers=None)
     print(results.get_response())
 
     # statistics = triton_client.get_inference_statistics(model_name=model_name,
